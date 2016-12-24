e0f549528ae4496de0dad6c48affa729053e5d79
Halley0624 <helijun240659077@gmail.com>
2016-12-24 10:16:07 +0100

    Contact view

---
+++ application/views/contact_view.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+
+  <title>Contact | Simple Life</title>
+  
+  
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("css/content/html.css"); ?>" media="screen, projection, tv " />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("css/content/layout.css"); ?>" media="screen, projection, tv" />
+ 
+ </head>
+ 
+ <body>
+ 
+ <!-- #wrapper: centers the content and sets the width -->
+<div id="wrapper">
+
+  <!-- #content: applies the white, dropshadow background -->
+  <div id="content">
+
+    <!-- #header: holds site title and subtitle -->
+    <div id="header">
+      <h1><span class="big darkBrown">S</span>imple <span class="big darkBrown">L</span>ife</h1>
+      <h2><span class="highlight">Because sometimes you don't need a sidebar</span></h2>
+    </div>
+    
+    
+     <!-- #menu: topbar menu of the site.  Use the helper classes .two, .three, .four and .five to set
+                the widths for 2, 3, 4 and 5 item menus. -->
+    <ul id="menu" class="four">
+      <li><a href="<?php echo base_url();?>index.php/change/showIndex" title="An intro to Simple Life"><span class="big">I</span>ntro</a></li>
+      <li><a href="<?php echo base_url();?>index.php/change/showContent" title="Coding tips"><span class="big">C</span>ontent</a></li>
+      <li><a href="<?php echo base_url(); ?>index.php/change/showWrite" title="See the tags in action" class="here"><span class="big">W</span>rite</a></li>
+      <li><a href="<?php echo current_url(); ?>" title="Get in touch" class="here"><span class="big">C</span>ontact</a></li>
+    </ul>
+
+    <!-- #page: holds all page content, as well as footer -->
+    <div id="page">
+    
+    <p>Lijun He<br/>
+    	Tell:(+49)1631710669<br/>
+    	Apartment 1004<br/>
+		Maria-Mitchell-Strasse 2-4<br/>
+		23562 Luebeck
+    </p>
+    
+    
+    <p>Zeling Wu<br/>
+    	Tell:(+49)1631710664<br/>
+    	Apartment 1206<br/>
+		Maria-Mitchell-Strasse 2-4<br/>
+		23562 Luebeck
+    </p>
+    
+    
+    <p>Zikai Zhou<br/>
+    	Tell:(+49)15779808837<br/>
+    	Apartment 1212<br/>
+		Maria-Mitchell-Strasse 2-4<br/>
+		23562 Luebeck
+    </p>
+    
+     </div>
+
+  </div>
+
+</div>
+ 
+</body>
+ 
+</html>
