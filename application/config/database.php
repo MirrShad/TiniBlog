e90b3bc71e90f387a7b94649aee191d0419dc38b
AndyWU <AndyWuIT@google.com>
2016-12-24 15:52:20 +0100

    sqlite database

--- application/config/database.php
+++ application/config/database.php
@@ -74,15 +74,15 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'test',
-	'dbdriver' => 'mysqli',
+	'dsn'   => '',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+	'database' => APPPATH.'database\TiniBlog.db',//'test',
+	'dbdriver' => 'sqlite3',//'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => FALSE,//(ENVIRONMENT !== 'production'),
+	'db_debug' => TRUE,//(ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
@@ -94,6 +94,38 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+/*
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'test',
+	'dbdriver' => 'mysqli',
+	'dsn'   => '',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+	'database' => APPPATH.'database\TiniBlog.db',//'test',
+	'dbdriver' => 'sqlite3',//'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => FALSE,//(ENVIRONMENT !== 'production'),
+	'db_debug' => TRUE,//(ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+*/
+
 
 
 
